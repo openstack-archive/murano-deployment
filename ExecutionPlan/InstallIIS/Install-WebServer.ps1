85f582cb341932d61217eb288dd88612ea1dee8e
Dmitry Teselkin <dteselkin@mirantis.com>
2013-06-17 19:41:08 +0400

    InstallIIS fixed to reflect latest changes in CoreFunctions and workflow.wq
    Change-Id: Iaa60283b6e6256829fafe4e817e95d7e643b1f72

--- ExecutionPlan/InstallIIS/Install-WebServer.ps1
+++ ExecutionPlan/InstallIIS/Install-WebServer.ps1
@@ -4,6 +4,8 @@ function Copy-Prerequisites {
 		[String] $Destination = ''
 	)
 
+	Write-Log "--> Copy-Prerequisites"
+
     if ($Destination -eq '') {
         throw("Copy-Prerequisites: Destination path not specified!")
     }
@@ -15,19 +17,27 @@ function Copy-Prerequisites {
         }
     }
 
-	New-PSDrive -Name 'P' -PSProvider 'FileSystem' -Root $Path
-	$null = New-Item -Path $Destination -ItemType Container -Force
-	Copy-Item -Path 'P:\Prerequisites\IIS' -Destination $Destination -Recurse -Force
-	Remove-PSDrive -Name 'P' -PSProvider 'FileSystem' -Force
+	Write-Log "Creating new PSDrive ..."
+	New-PSDrive -Name 'P' -PSProvider 'FileSystem' -Root $Path | Out-Null
+	Write-Log "Creating destination folder ..."
+	New-Item -Path $Destination -ItemType Container -Force | Out-Null
+	Write-Log "Copying items ..."
+	Copy-Item -Path 'P:\Prerequisites\IIS' -Destination $Destination -Recurse -Force | Out-Null
+	Write-Log "Removing PSDrive ..."
+	Remove-PSDrive -Name 'P' -PSProvider 'FileSystem' -Force | Out-Null
+	
+	Write-Log "<-- Copy-Prerequisites"
 }
 
 
 
-function Install-WebServerComponents {
+function Install-WebServer {
 	param (
 		[String] $PrerequisitesPath
 	)
 	
+	Write-Log "--> Install-WebServerComponents"
+
 	$FeatureList = @(
 		'Web-Server',
 		'Web-Net-Ext45',
@@ -45,6 +55,7 @@ function Install-WebServerComponents {
     
 	$PrerequisitesPath = [IO.Path]::Combine($PrerequisitesPath, 'IIS')
     
+	Write-Log "Validating prerequisites based on the list ..."
 	foreach ($FileName in $PrerequisitesList) {
 		$FilePath = [IO.Path]::Combine($PrerequisitesPath, $FileName)
 		if (-not (Test-Path -Path $FilePath -PathType Leaf)) {
@@ -54,20 +65,25 @@ function Install-WebServerComponents {
 	
 	Import-Module ServerManager
 	
+	Write-Log "Installing Web Server ..."
 	Install-WindowsFeature $FeatureList -IncludeManagementTools
 	
-	$Exec = Exec -FilePath [IO.Path]::Combine($PrerequisitesPath, 'AspNetMvc4Setup.exe') -ArgumentList '/q'
+	Write-Log "Installing AspNetMvp4 ..."
+	$Exec = Exec -FilePath $([IO.Path]::Combine($PrerequisitesPath, 'AspNetMvc4Setup.exe')) -ArgumentList '/q' -PassThru
 	if ($Exec.ExitCode -ne 0) {
 		throw("Installation of 'AspNetMvc4Setup.exe' failed. Process exit code '$($Exec.ExitCode)'")
 	}
 	
 	# Extract WebApplications folder with *.target files to
 	#   C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v10.0
+	Write-Log "Installing WebApplication targets ..."
 	$WebApplicationsTargetsRoot = 'C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v10.0'
 	$null = New-Item -Path $WebApplicationsTargetsRoot -ItemType Container
-	$Exec = Exec -FilePath [IO.Path]::Combine($PrerequisitesPath, 'WebApplications.exe') -ArgumentList @("-o`"$WebApplicationsTargetsRoot`"", '-y')
+	$Exec = Exec -FilePath $([IO.Path]::Combine($PrerequisitesPath, 'WebApplications.exe')) -ArgumentList @("-o`"$WebApplicationsTargetsRoot`"", '-y') -PassThru
 	if ($Exec.ExitCode -ne 0) {
 		throw("Installation of 'WebApplications.exe' failed. Process exit code '$($Exec.ExitCode)'")
 	}
+
+	Write-Log "<-- Install-WebServerComponents"
 }
 
