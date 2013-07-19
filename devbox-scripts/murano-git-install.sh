0cfcb5e091e2ba05884cac742f92406d52040fb2
Dmitry Teselkin <dteselkin@mirantis.com>
2013-07-19 15:34:06 +0400

    murano-git-install updated.
    Change-Id: I8185e654fa28f767afb1feb6773caa97b4985f3c

--- devbox-scripts/murano-git-install.sh
+++ devbox-scripts/murano-git-install.sh
@@ -29,7 +29,8 @@ os_version=''
 ##################################################
 
 
-
+# Helper funtions
+#-------------------------------------------------
 function die {
 	printf "\n==============================\n"
 	printf "$@"
@@ -53,93 +54,86 @@ function iniset {
 		sed -i -e "/^\[$section\]/,/^\[.*\]/ s|^\($option[ \t]*=[ \t]*\).*$|\1$value|" "$file"
 	fi
 }
+#-------------------------------------------------
 
 
-
-mkdir -p $git_clone_root
-
-if [ -f /etc/redhat-release ] ; then
-	os_version=$(cat /etc/redhat-release | cut -d ' ' -f 1)
-fi
-
-if [ -f /etc/lsb-release ] ; then
-	os_version=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d '=' -f 2)
-fi
-
-if [ -z $os_version ] ; then
-	die "Unable to determine OS version. Exiting." 
-else
-	log "* OS version is '$os_version'"
-fi
-
-
-configuration_required='false'
-for config_file in $murano_config_files ; do
-	if [ ! -f "$config_file" ] ; then
-		log "! Required config file '$config_file' not exists. Murano should be configured before start."
-		configuration_required='true'
-	fi
-done
-
-
-for app_name in $murano_components ; do
-	log "* Working with '$app_name'"
-
-	git_repo="$git_prefix/$app_name.git"
-	git_clone_dir="$git_clone_root/$app_name"
-
-	if [ ! -d "$git_clone_dir" ] ; then
-		git clone $git_repo $git_clone_dir || die "Unable to clone repository '$git_repo'"
-		up_to_date='false'
-	else
-		cd "$git_clone_dir"
-		git reset --hard
-		git clean -fd
-		git remote update
-		git checkout master
-
-		rev_on_local=$(git rev-list --max-count=1 master)
-		rev_on_origin=$(git rev-list --max-count=1 origin/master)
-
-		if [ "$rev_on_local" == "$rev_on_origin" ] ; then
-			up_to_date='true'
-		else
-			git pull origin master
-			up_to_date='false'
+# Workflow functions
+#-------------------------------------------------
+function install_murano {
+	configuration_required='false'
+	for config_file in $murano_config_files ; do
+		if [ ! -f "$config_file" ] ; then
+			log "! Required config file '$config_file' not exists. Murano should be configured before start."
+			configuration_required='true'
 		fi
-	fi
+	done
 
-	if [ "$up_to_date" == 'false' ] ; then
-		chmod +x "$git_clone_dir"/setup*.sh
-		
-		log "* Uninstalling '$app_name' ..."
-		case $os_version in
-			'CentOS')
-				"$git_clone_dir"/setup-centos.sh uninstall
-			;;
-			'Ubuntu')
-				"$git_clone_dir"/setup.sh uninstall
-			;;
-		esac
-		
-		sleep 2
-		
-		log "* Installing '$app_name' ..."
-		case $os_version in
-			'CentOS')
-				"$git_clone_dir"/setup-centos.sh install
-			;;
-			'Ubuntu')
-				"$git_clone_dir"/setup.sh install
-			;;
-		esac
-	fi
-done
+	for app_name in $murano_components ; do
+		log "* Working with '$app_name'"
+
+		git_repo="$git_prefix/$app_name.git"
+		git_clone_dir="$git_clone_root/$app_name"
+
+		if [ ! -d "$git_clone_dir" ] ; then
+			git clone $git_repo $git_clone_dir || die "Unable to clone repository '$git_repo'"
+			up_to_date='false'
+		else
+			cd "$git_clone_dir"
+			git reset --hard
+			git clean -fd
+			git remote update
+			git checkout master
+
+			rev_on_local=$(git rev-list --max-count=1 master)
+			rev_on_origin=$(git rev-list --max-count=1 origin/master)
+
+			if [ "$rev_on_local" == "$rev_on_origin" ] ; then
+				up_to_date='true'
+			else
+				git pull origin master
+				up_to_date='false'
+			fi
+		fi
+
+		if [ "$up_to_date" == 'false' ] ; then
+			chmod +x "$git_clone_dir"/setup*.sh
+			
+			log "* Uninstalling '$app_name' ..."
+			case $os_version in
+				'CentOS')
+					"$git_clone_dir"/setup-centos.sh uninstall
+				;;
+				'Ubuntu')
+					"$git_clone_dir"/setup.sh uninstall
+				;;
+			esac
+			
+			sleep 2
+			
+			log "* Installing '$app_name' ..."
+			case $os_version in
+				'CentOS')
+					"$git_clone_dir"/setup-centos.sh install
+				;;
+				'Ubuntu')
+					"$git_clone_dir"/setup.sh install
+				;;
+			esac
+		fi
+	done
 
 
-if [ "$configuration_required" == 'true' ] ; then
-	if [ ! -f '/etc/murano-deployment/lab-binding.rc' ] ; then
+	if [ "$configuration_required" == 'true' ] ; then
 		log "One or several configuraiton files were not found before installation was launched."
+		configure_murano
+	fi
+
+	restart_murano
+}
+
+
+function configure_murano {
+	if [ ! -f '/etc/murano-deployment/lab-binding.rc' ] ; then
 		log "Create '/etc/murano-dashboard/lab-binding.rc' or configure services individually."
 		die "Murano components require configuration."
 	fi
@@ -159,26 +153,56 @@ if [ "$configuration_required" == 'true' ] ; then
 				iniset 'rabbitmq' 'virtual_host' "$RABBITMQ_VHOST" "$config_file"
 			;;
 			'/etc/murano-conductor/conductor.conf')
-				iniset 'filter:authtoken' 'auth_host' "$LAB_HOST" "$config_file"
-				iniset 'filter:authtoken' 'admin_user' "$ADMIN_USER" "$config_file"
-				iniset 'filter:authtoken' 'admin_password' "$ADMIN_PASSWORD" "$config_file"
-			;;
-			'/etc/murano-conductor/conductor-paste.ini')
 				iniset 'heat' 'auth_url' "$AUTH_URL" "$config_file"
 				iniset 'rabbitmq' 'host' "$LAB_HOST" "$config_file"
 				iniset 'rabbitmq' 'login' "$RABBITMQ_LOGIN" "$config_file"
 				iniset 'rabbitmq' 'password' "$RABBITMQ_PASSWORD" "$config_file"
 				iniset 'rabbitmq' 'virtual_host' "$RABBITMQ_VHOST" "$config_file"
 			;;
+			'/etc/murano-conductor/conductor-paste.ini')
+				iniset 'filter:authtoken' 'auth_host' "$LAB_HOST" "$config_file"
+				iniset 'filter:authtoken' 'admin_user' "$ADMIN_USER" "$config_file"
+				iniset 'filter:authtoken' 'admin_password' "$ADMIN_PASSWORD" "$config_file"
+			;;
 			'/etc/openstack-dashboard/local_settings')
 				iniset '' 'OPENSTACK_HOST' "$LAB_HOST" "$config_file"
 			;;
 		esac
 	done
+}
+
+
+function restart_murano {
+	for service_name in $murano_services ; do
+		stop "$service_name"
+		start "$service_name"
+	done
+}
+#-------------------------------------------------
+
+
+mkdir -p $git_clone_root
+
+if [ -f /etc/redhat-release ] ; then
+	os_version=$(cat /etc/redhat-release | cut -d ' ' -f 1)
+fi
+
+if [ -f /etc/lsb-release ] ; then
+	os_version=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d '=' -f 2)
+fi
+
+if [ -z $os_version ] ; then
+	die "Unable to determine OS version. Exiting." 
+else
+	log "* OS version is '$os_version'"
 fi
 
 
-for service_name in $murano_services ; do
-	stop "$service_name"
-	start "$service_name"
-done
+case $mode in
+	'install')
+		install_murano
+	;;
+	'configure')
+		configure_murano
+	;;
+esac
