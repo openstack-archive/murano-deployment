1390e4ba18dd9d95401543aa58acc4089eed4052
Dmitry Teselkin <dteselkin@mirantis.com>
2013-07-30 13:50:29 +0400

    Failover Cluster scripts updated.
    Change-Id: I909d7a36b62074d05ad6ddb14afc392d0408cf15

--- WindowsPowerShell/Functions/Failover-Cluster.ps1
+++ WindowsPowerShell/Functions/Failover-Cluster.ps1
@@ -1,49 +1,44 @@
 <#
 .DESCRIPTION
 
-Failover Cluster Input Data (from the UI):
+## Failover Cluster Input Data (from the UI)
 
 * Domain Membership
-    - [String] or [Select box]: Domain name
+    - [String] / [Select box] $DomainName - Domain name
 * Domain User Credentials
-    - [String]: Username
-    - [Password string]: Password
+    - [String] $UserName - Username
+    - [Password string] $UserPassword - User password
 * Shared Folder Information
-    - [String]: Server which will host the folder
-    - [String]: Share name
-    - [String]: Shared folder internal path
+    - [String] $ShareServer - Server which will host the folder
+    - [String] $ShareName - Share name
+    - [String] $SharePath - Shared folder internal path
 * Failover Cluster Members
-    - [String]: Cluster name
-    - [String[]]: List of node names
+    - [String] $ClusterName - Cluster name
+    - [String] $ClusterIP - Static IP address that will be assigned to the cluster
+    - [String[]] $ClusterNodes - List of node names
 
-Failover Cluster creation workflow:
+
+
+## Failover Cluster creation workflow
 
 * Create AD domain
 * Join all the VMs to that domain
 * Prepare nodes
     - Install Failover Cluster prerequisites on all FC nodes
-    - Disable or configure firewall (?)
 * Create failover cluster
     - Create new cluster
     - Add members
-* Create shared folder
-    - Create new folder
-    - Share that folder with appropriate permissions
 * Confugure FC quorum
+    - Create new folder that will be shared
+    - Share that folder with appropriate permissions
     - Configure quorum mode
 
 
 
-Helpful SmbShare* Functions:
+## Helpful SmbShare* Functions
 
 * New-SmbShare
 * Grant-SmbShareAccess
-
-
-TODO
-====
-
-[-] Script to create shared folder for FC (New-FailoverClusterSharedFolder)
 
 #>
 
@@ -104,74 +99,136 @@ function Start-PowerShellProcess {
 
 function Install-FailoverClusterPrerequisites {
     Import-Module FailoverClusters
-    
-	Add-WindowsFeature Failover-Clustering, RSAT-Clustering-PowerShell
+
+    Add-WindowsFeature Failover-Clustering, RSAT-Clustering-PowerShell
 }
 
 
-<#
+
 function New-FailoverClusterSharedFolder {
 	param (
-		[String] $Path,
-		[String] $ShareName,
+        [String] $ClusterName,
         [String] $DomainName,
-		[String] $ClusterName
+        [String] $ShareServer,
+		[String] $SharePath,
+		[String] $ShareName,
+        [String] $UserName,
+        [String] $UserPassword
+        $Credential = $null
 	)
     
-    New-Item -Path $Path -ItemType Container -Force
-    
-    New-SmbShare -Path $Path `
-        -Name $ShareName `
-        -FullAccess "$DomainName\$ClusterName`$" `
-        -Description "Shared folder for Failover Cluster."
+    if ($Credential -eq $null) {
+        $Credential = New-Credential -UserName "$DomainName\$UserName" -Password "$UserPassword"
+    }
+
+    if ((Test-Connection -ComputerName $ShareServer -Count 1 -Quiet) -eq $false) {
+        throw("Server '$ShareServer' is unreachable via ICMP.")
+    }
+
+    $Session = New-PSSession -ComputerName $ShareServer -Credential $Credential
+
+    Invoke-Command -Session $Session -ScriptBlock {
+            param (
+                [String] $SharePath,
+                [String] $ShareName,
+                [String] $ClusterAccount
+            )
+
+            New-Item -Path $SharePath -ItemType Container -Force
+            
+            New-SmbShare -Path $SharePath `
+                -Name $ShareName `
+                -FullAccess "$ClusterAccount" `
+                -Description "Shared folder for Failover Cluster."
+
+        } -ArgumentList $SharePath, $ShareName, "$DomainName\$ClusterName`$"
+
+    Set-ClusterQuorum -NodeAndFileShareMajority "\\$ShareServer\$ShareName"
 }
-#>
+
 
 
 function New-FailoverCluster {
 	param (
-		[String] $Name,
+		[String] $ClusterName,
 		[String] $StaticAddress,
-		[String[]] $Members,
-		[String] $SharedFolder,
+		[String[]] $ClusterNodes,
+        [String] $UserName,
+        [String] $UserPassword
         $Credential
 	)
+
+    if ($Credential -eq $null) {
+        $Credential = New-Credential -UserName "$DomainName\$UserName" -Password "$UserPassword"
+    }
+
     Import-Module FailoverClusters
 
-	if ((Get-Cluster $Name -ErrorAction SilentlyContinue) -eq $null) {
-        Write-Log "Creating new cluster '$Name' ..."
+	if ((Get-Cluster $ClusterName -ErrorAction SilentlyContinue) -eq $null) {
+        Write-Log "Creating new cluster '$ClusterName' ..."
         Start-PowerShellProcess -Command @"
 Import-Module FailoverClusters
-New-Cluster -Name '$Name' -StaticAddress '$StaticAddress'
+New-Cluster -Name '$ClusterName' -StaticAddress '$StaticAddress'
 "@ -Credential $Credential
         Start-Sleep -Seconds 15
     }
     else {
-        Write-Log "Cluster '$Name' already exists."
+        Write-Log "Cluster '$ClusterName' already exists."
     }
 
-    foreach ($Node in $Members) {
+    foreach ($Node in $ClusterNodes) {
         if ((Get-ClusterNode $Node -ErrorAction SilentlyContinue) -eq $null) {
-            Write-Log "Adding node '$Node' to the cluster '$Name' ..."
+            Write-Log "Adding node '$Node' to the cluster '$ClusterName' ..."
             Start-PowerShellProcess -Command @"
 Import-Module FailoverClusters
 Add-ClusterNode -Cluster '$Name' -Name '$Node'
 "@ -Credential $Credential
         }
         else {
-            Write-Log "Node '$Node' already a part of the cluster '$Name'."
+            Write-Log "Node '$Node' already a part of the cluster '$ClusterName'."
         }
     }
-
-    #Set-ClusterQuorum -NodeAndFileShareMajority $SharedFolder
 }
 
 
 
 <#
+
+# Example
+
+$DomainName = 'fc-acme.local'
+$DomainUser = 'Administrator'
+$DomainPassword = 'P@ssw0rd'
+
+$ClusterName = 'fc-test'
+$ClusterIP = '10.200.0.60'
+$ClusterNodes = @('fc-node-01','fc-node-02','fc-node-03')
+
+$ShareServer = 'fc-dc-01'
+$ShareName = 'FCShare'
+
+$SharePath = "C:\$ShareName"
+
+
+
 Import-Module CoreFunctions -Force
 
-$Creds = New-Credential -UserName 'fc-acme\Administrator' -Password 'P@ssw0rd'
+$Creds = New-Credential `
+    -UserName "$DomainName\$DomainUser" `
+    -Password "$DomainPassword"
 
-New-FailoverCluster -Name 'fc-test-3' -StaticAddress '10.200.0.60' -Members 'fc-node-01','fc-node-02','fc-node-03' -Credential $Creds
+New-FailoverCluster `
+    -ClusterName $ClusterName `
+    -StaticAddress $ClusterIP `
+    -ClusterNodes $ClusterNodes `
+    -Credential $Creds
+
+New-FailoverClusterSharedFolder `
+    -ClusterName $ClusterName `
+    -DomainName $DomainName `
+    -ShareServer $ShareServer `
+    -SharePath "$SharePath" `
+    -ShareName "$ShareName" `
+    -Credential $Creds
+
 #>
