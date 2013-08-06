f4f0042a653217923a6142ca17a267136e3234d4
Dmitry Teselkin <dteselkin@mirantis.com>
2013-08-06 17:21:18 +0400

    Show-InvocationInfo added to Logger.ps1.
    This function helps to trace function calls.
    
    Change-Id: I4a8a2469c109747e32220f882bb88e73291a31e8

--- WindowsPowerShell/Modules/CoreFunctions/include/Logger.ps1
+++ WindowsPowerShell/Modules/CoreFunctions/include/Logger.ps1
@@ -134,4 +134,29 @@ New-Alias -Name Write-LogDebug -Value Out-LogDebug
 
 
 
+function Show-InvocationInfo {
+    param (
+        $Invocation,
+        [Switch] $End
+    )
+
+    if ($End) {
+        Write-LogDebug "</function name='$($Invocation.MyCommand.Name)'>"
+    }
+    else {
+        Write-LogDebug "<function name='$($Invocation.MyCommand.Name)'>"
+        Write-LogDebug "<param>"
+        foreach ($Parameter in $Invocation.MyCommand.Parameters) {
+            foreach ($Key in $Parameter.Keys) {
+                $Type = $Parameter[$Key].ParameterType.FullName
+                foreach ($Value in $Invocation.BoundParameters[$Key]) {
+                    Write-LogDebug "[$Type] $Key = '$Value'"
+                }
+            }
+        }
+        Write-LogDebug "</param>"
+    }
+}
+
+
 Initialize-Logger
