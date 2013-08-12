88ff81bf6716212c9b8c7f3e6630fb089ad64110
Serg Melikyan <smelikyan@mirantis.com>
2013-08-12 16:46:42 +0400

    Rewritten and commited docs-builder
    Change-Id: I0e01c3716765c2733ad3106ac6792c9f4059506a

---
+++ docs-builder/builder.sh
@@ -0,0 +1,40 @@
+#clone and clean github pages
+rm -rf gh-pages
+git clone -b gh-pages git@github.com:Mirantis/murano-docs.git gh-pages
+cd gh-pages
+rm -rf '!(.git|.nojekyll|CNAME)'
+git add -A
+cd ..
+
+#clone Murano Docs
+rm -rf murano-docs
+git clone https://github.com/stackforge/murano-docs
+
+#copy site
+cp murano-docs/site/index.html ~/tests/gh-pages/
+
+#generate murano-manual
+cd murano-docs/src/murano-manual
+mvn clean generate-sources
+
+#copy murano-manual
+cp -r target/docbkx/webhelp/murano-manual/* ~/tests/gh-pages/docs/murano-manual
+cp target/docbkx/pdf/murano-manual.pdf ~/tests/gh-pages/docs/murano-manual
+cd ~/tests/gh-pages
+
+#generate murano-deployment-guide
+cd murano-docs/src/murano-deployment-guide
+mvn clean generate-sources
+
+#copy murano-manual
+cp -r target/docbkx/webhelp/murano-deployment-guide/* ~/tests/gh-pages/docs/murano-deployment-guide
+cp -r target/docbkx/pdf/murano-deployment-guide.pdf ~/tests/gh-pages/docs/murano-deployment-guide
+cd ~/tests/gh-pages
+
+#commit generated data
+cd ~/tests/gh-pages
+git config user.email "tnurlygayanov@mirantis.com"
+git config user.name "Timur Nurlygayanov"
+git add .
+git commit -am "generated `date`."
+git push origin gh-pages
