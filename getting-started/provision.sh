2dc524f73ddc8d978b94b787b34b98d2a3fa7ec8
Dmitry Teselkin <dteselkin@mirantis.com>
2013-09-04 14:52:03 +0400

    Folder for Murano Getting Started created.
    * README updated.
    * Files to start Vagrant box added.
    * devstack's files added.
    * Permissions on local.sh changed.
    * devstack's files updated.
    
    Change-Id: I07b2562545e891490bdc6e2d41a804aad8f3ba7f

---
+++ getting-started/provision.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+function log {
+   echo "$@" | tee --append ~/provision.log
+}
+
+apt-get install -y git
+
+if [ ! -f '/vagrant/lab-binding.rc' ] ; then
+	echo "File '/vagrant/lab-binding.rc' not found!"
+	exit 1
+fi
+
+mkdir /etc/murano-deployment
+
+if [ ! -f '/etc/murano-deployment/lab-binding.rc' ] ; then
+    cp /vagrant/lab-binding.rc /etc/murano-deployment
+fi
+
+mkdir /opt/git
+
+cd /opt/git
+
+log "Cloning the 'murano-deployment' repository ..."
+git clone https://github.com/stackforge/murano-deployment.git >> ~/provision.log
+
+cd murano-deployment
+git checkout -b release-0.2 origin/release-0.2
+
+log "Installing pip ..."
+apt-get install python-setuptools >> ~/provision.log
+easy_install pip                  >> ~/provision.log
+
+cd devbox-scripts
+log "Installing murano prerequisites ..."
+./murano-git-install.sh prerequisites >> ~/provision.log
+
+log "Installing murano components ..."
+./murano-git-install.sh install       >> ~/provision.log
+
