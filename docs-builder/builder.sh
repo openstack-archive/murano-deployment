954549a62a7079a9bf8e90d03ece6cfcd33e1215
Timur Sufiev <tsufiev@mirantis.com>
2013-09-10 14:41:58 +0400

    Cherry-pick all changes from release 0.2.
    * Updated puppet recepies.
      Iaacba512063811f753a2db768858b22b73f3b9c7
    * Updated documentation
      Id2384b2226a4932963ebc9f6185a9e0cdea869d0
    * murano-git-install.sh fix
      lab-binding.rc file now checked and created before any other operations.
      I0475e4adeb8c1c568125dce4b773bcf857b8cc3f
    * Support to use branches not merged yet.
      Additional action 'upgrade' added. Now 'update' only fetches changes
      from repo. murano-git-install.sh now can fetch and install from branches
      that were not merged yet. This should simplify testing and developing.
      If10e830d46c8af58a26b242b576beaa66f8bc1b6
    * Fixed error with 'no branch' in murano-git-install.
      Log messages updated.
      I6e56c0bbc0c2b1d997969dc03b0e580f7ec24577
    * Checkout of target branch added after clone new repo.
      python-muranoclient removed from list of installed components.
      I4afa028e72a1b82bbb81bc92b69ea7c0a62c4d20
    * Ability to checkout ref branch added.
      Iab9a88621283a23063a4c3c558ee3fb31a29514c
    * murano-git-install updated
      Additional packages added. Firewall disables. SELinux disabled
      If9449174eaecc3d4736a3bb13ef4420b01256659
    * Murano configuration parameters.
      'log_file' and 'auth_url' configuration parameters are updated
      during install
      I3af022db21af8a19de6fefed1d2daaeebd558a4c
    * SSL Support added to murano-git-install script.
      Addresses blueprint https://blueprints.launchpad.net/murano/+spec/ssl
      Ib9eb1b8727416a7d67a51277b6493c9e58f7c3bd
    * Avoiding error message '[: too many arguments'
      I0253723078f5c3e77debfd8a8138559878544959
    * Test expression fixed.
      Using test statement [...] was wrong for this type of check.
      Fixes: MRN-903
      Ie6640189206084c89f665478d93ef2444afcfd5b
    * Update script for building documentation
      Now documentation built not from source checkouted by Jenkins, but
      checked out explicitly from script. Also we checking out, building
      and publishing several versions at once. No landing page now
      exists.
      Ifbb6d989632ad2277bc1194bd9b440597c916851
    * Folder for Murano Getting Started created.
      README updated, Files to start Vagrant box added, devstack's files
      added, permissions on local.sh changed, devstack's files updated.
      I07b2562545e891490bdc6e2d41a804aad8f3ba7f
    * Fixed compatibility with Sh
      I077d59f2a864ee8ccace72c35019be17680b8267
    * Added feature to build master
      Ib02ddf2a4c5eefb851a1ce12ce515ef4b813bbc1
    * Renamed murano-manual developers-guide
      I55332c02533c42ce0ece8d89867180daca48b781
    * Three occurences of 'bad' character replaced.
      Fixes: MRN-947
      I2f14b97232f0ad772a339cd7d0ebf25e831a76cf
    * Resolved issues with different guide names for different vers
      If487a82bd1d68a13577b3c640117f6c737f48885
    * Correct pip version now installing by murano-git-install.
      Bug #1221256
      I837ba3af097f7f59123abdd99d9dc7029568ce4d
    * Fixed list of guides for v0.2
      I62240939c742cfc18527b5e79ef578a8bee7224a
    * Default branch name fixed.
      Iaac43376f9e3514fe924920035a5a779a303ff11
    
    Also Change default install branch to master.
    
    Change-Id: I7bb689f563f6b9806ff1d8dfd4b2161201820798

--- docs-builder/builder.sh
+++ docs-builder/builder.sh
@@ -1,42 +1,68 @@
 #!/bin/sh -x
+#    Copyright (c) 2013 Mirantis, Inc.
 #
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-cd ~/tests
+#create temp directory where we going to work
+TEMP=$PWD/temp-$(date +%s)
+mkdir "${TEMP}"
 
 #clone and clean github pages
-rm -rf murano-docs
+cd "${TEMP}"
 git clone git@github.com:murano-docs/murano-docs.github.io.git murano-docs
 cd murano-docs
 ls -A1 | grep -v -e '\.git' | xargs git rm -rf
-cd ~/tests
 
-#copy site
-cp -r $WORKSPACE/site/* ~/tests/murano-docs/
+for version in "0.1" "0.2" "latest"
+do
+    cd "${TEMP}"
 
-#generate murano-manual
-cd $WORKSPACE/src/murano-manual
-mvn clean generate-sources
+    if [ ${version} = "latest" ]; then
+        guides="murano-manual murano-deployment-guide"
+        branch="master"
+    elif [ ${version} = "0.2" ]; then
+        guides="developers-guide administrators-guide user-guide"
+        branch="release-${version}"
+    elif [ ${version} = "0.1" ]; then
+        guides="murano-manual murano-deployment-guide"
+        branch="release-${version}"
+    else
+        guides="developers-guide murano-deployment-guide"
+        branch="release-${version}"
+    fi
 
-#copy murano-manual
-mkdir -p ~/tests/murano-docs/docs/murano-manual
-cp -r target/docbkx/webhelp/murano-manual/* ~/tests/murano-docs/docs/murano-manual
-cp target/docbkx/pdf/murano-manual.pdf ~/tests/murano-docs/docs/murano-manual
-cd ~/tests
+    git clone -b ${branch} git@github.com:stackforge/murano-docs.git docs-${version}
+    cd docs-${version} && git pull && cd ..
 
-#generate murano-deployment-guide
-cd $WORKSPACE/src/murano-deployment-guide
-mvn clean generate-sources
+    for guide in ${guides}
+    do
+        cd "${TEMP}/docs-${version}/src/${guide}"
+        mvn clean generate-sources
 
-#copy murano-deployment-guide
-mkdir -p ~/tests/murano-docs/docs/murano-deployment-guide
-cp -r target/docbkx/webhelp/murano-deployment-guide/* ~/tests/murano-docs/docs/murano-deployment-guide
-cp -r target/docbkx/pdf/murano-deployment-guide.pdf ~/tests/murano-docs/docs/murano-deployment-guide
-cd ~/tests
+        built_manual=${TEMP}/murano-docs/${version}/${guide}
+        mkdir -p "${built_manual}"
+        cp -r "target/docbkx/webhelp/${guide}"/* "${built_manual}"
+        cp "target/docbkx/pdf/${guide}.pdf" "${built_manual}"
+    done
+done
 
 #commit generated data
-cd ~/tests/murano-docs
+cd "${TEMP}/murano-docs"
 git config user.email "murano-eng@mirantis.com"
 git config user.name "murano-docs"
 git add .
 git commit -am "generated `date`."
 git push origin master
+
+#clean-up
+rm -rf "${TEMP}"
