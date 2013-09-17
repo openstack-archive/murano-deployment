59ec24e87a02d0c2f4706116d6c71c9504bb5335
Ekaterina Fedorova <efedorova@mirantis.com>
2013-09-17 14:39:36 +0400

    Update openstack_dashboard prerequirements on Centos
    - Sync with openstack docs: http://docs.openstack.org/trunk/openstack-compute/install/yum/content/installing-openstack-dashboard.html
    
    - Update version defenition way
    
    Change-Id: I69aa0d07cacb2ed0c2b8d8454bd4e8b21dca7bec

--- devbox-scripts/murano-git-install.sh
+++ devbox-scripts/murano-git-install.sh
@@ -104,6 +104,33 @@ function generate_sample_certificate {
 
     cd $old_pwd
 }
+
+function get_distro_name()
+{
+    local n
+    if [[ -r '/etc/lsb-release' ]]; then
+        . /etc/lsb-release
+        [[ "$DISTRIB_ID" ]] && n="$DISTRIB_ID"
+    elif [[ -r '/etc/release' ]]; then
+        n=`head -1 /etc/release | sed 's/ *\([[^0-9]]*\) [0-9].*/\1/'`
+    elif [[ -r '/etc/arch-release' ]]; then
+        n="ArchLinux"
+    elif [[ -r '/etc/debian_version' ]]; then
+        n='Debian'
+    elif [[ -r '/etc/gentoo-release' ]]; then
+        n='Gentoo'
+    elif [[ -r '/etc/fedora-release' ]]; then
+        n='Fedora'
+    elif [[ -r '/etc/centos-release' ]]; then
+        n='CentOS'
+    elif [[ -r '/etc/redhat-release' ]]; then
+        n='Redhat'
+    elif [[ -r '/etc/SuSE-release' ]]; then
+        n='SuSE'
+    fi
+    echo "$n"
+}
+
 #-------------------------------------------------
 
 
@@ -125,7 +152,7 @@ function install_prerequisites {
             #ln -s /usr/local/bin/pip /usr/bin/pip
 
             log "** Installing OpenStack dashboard ..."
-            yum install make gcc python-netaddr.noarch python-keystoneclient.noarch python-django-horizon.noarch python-django-openstack-auth.noarch  httpd.x86_64 mod_wsgi.x86_64 openstack-dashboard.noarch --assumeyes
+            yum install make gcc yum install make gcc memcached python-memcached mod_wsgi openstack-dashboard python-netaddr.noarch --assumeyes
 
             log "** Disabling firewall ..."
             service iptables stop
@@ -449,13 +476,7 @@ fi
 
 mkdir -p $git_clone_root
 
-if [ -f /etc/redhat-release ] ; then
-    os_version=$(cat /etc/redhat-release | cut -d ' ' -f 1)
-fi
-
-if [ -f /etc/lsb-release ] ; then
-    os_version=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d '=' -f 2)
-fi
+os_version=$(get_distro_name)
 
 if [ -z $os_version ] ; then
     die "Unable to determine OS version. Exiting."
@@ -470,6 +491,9 @@ case $os_version in
     ;;
     'Ubuntu')
         murano_config_files="$murano_config_files /etc/openstack-dashboard/local_settings.py"
+    ;;
+    *)
+    die "Sorry your OS version is not supported yet. Try Centos or Ubuntu."
     ;;
 esac
 
