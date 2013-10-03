d1d7873503aca2b8605d580f0880f7ecfb17e5bf
Dmitry Teselkin <dteselkin@mirantis.com>
2013-10-03 12:27:28 +0400

    CirrOS Image Builder updated
    Change-Id: I4fe9abcaf6703f18fc0fbf18b2d5998eb9597b9a

--- cirros/build-murano-cirros.sh
+++ cirros/build-murano-cirros.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -o xtrace
+set -o errexit
+
 arch=${1:-i386}
 # Image Builder Root
 IBR=${IBR:-/tmp/murano-cirros/$arch}
@@ -15,15 +18,18 @@ $@
 "
 }
 
+apt-get install kpartx |:
+
+mkdir -p $IBR |:
 
 case $arch in
   i386)
-	wget -P $IBR -O cirros.img https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-i386-disk.img
-	wget -P $IBR -O murano-agent https://www.dropbox.com/sh/zthldcxnp6r4flm/Os1Q9W5ZIx/murano-agent-i386
+	wget -O $IBR/cirros.img https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-i386-disk.img
+	wget -O $IBR/murano-agent https://www.dropbox.com/sh/zthldcxnp6r4flm/Os1Q9W5ZIx/murano-agent-i386
   ;;
   x86_64)
-	wget -P $IBR -O cirros.img https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img
-	wget -P $IBR -O murano-agent https://www.dropbox.com/sh/zthldcxnp6r4flm/7dsz0mMg1_/murano-agent-x86_64
+	wget -O $IBR/cirros.img https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img
+	wget -O $IBR/murano-agent https://www.dropbox.com/sh/zthldcxnp6r4flm/7dsz0mMg1_/murano-agent-x86_64
   ;;
   *)
 	die "Unsupported arch '$arch'"
@@ -33,13 +39,13 @@ esac
 
 qemu-img convert -O raw $IBR/cirros.img $IBR/cirros.raw
 
-mkdir /mnt/image
+mkdir /mnt/image |:
 
-[ -d /dev/$LOOP_DEV ] && \
+[ -n "$(losetup --all | grep $LOOP_DEV)" ] && \
 	die "Device /dev/$LOOP_DEV already exists."
 
 
-losetup /dev/$LOOP_DEV cirros.raw
+losetup /dev/$LOOP_DEV $IBR/cirros.raw
 kpartx -a /dev/$LOOP_DEV
 mount /dev/mapper/${LOOP_DEV}p1 /mnt/image
 
@@ -52,10 +58,12 @@ chmod 755 /mnt/image/usr/sbin/murano-agent
 chown root:root /mnt/image/usr/sbin/murano-agent
 
 cp ./murano-agent.init /mnt/image/etc/init.d/murano-agent
-chmod 755 /mnt/image/init.d/murano-agent
-chown root:root /mnt/image/init.d/murano-agent
-cd /mnt/image/rc3.d && ln -s S99-murano-agent ../init.d/murano-agent
+chmod 755 /mnt/image/etc/init.d/murano-agent
+chown root:root /mnt/image/etc/init.d/murano-agent
+cd /mnt/image/etc/rc3.d && ln -s ../init.d/murano-agent S99-murano-agent
 
+cd $IBR
+sleep 5
 
 umount /mnt/image
 kpartx -d /dev/$LOOP_DEV
