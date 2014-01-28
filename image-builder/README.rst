40310cf29161809a5ea7e5c689b99c7ff3a77fec
Dmitry Teselkin <dteselkin@mirantis.com>
2014-01-28 16:46:28 +0400

    Image builder updated
    * Installation script makes full system preparation
    * README updated
    
    Change-Id: If359789737221c1ab6a19d060d1c45dcf9866282

--- image-builder/README.rst
+++ image-builder/README.rst
@@ -3,25 +3,26 @@ Image builder
 
 Use the 'make' utility to start build new Windows image.
 
-During build preparation this folder will be copied entirely to the **/opt/image-builder** folder. We refer to that folder using variable **BUILD_ROOT**.
+During build preparation this folder will be copied entirely to the **/opt/image-builder** folder. In this document we refer to that folder using variable **IMAGE_BUILDER_ROOT**.
 
 Prerequisites
 -------------
-The following ISO files **MUST** be placed under **$BUILD_ROOT/libvirt/images** folder:
+The following ISO files **MUST** be placed under **$IMAGE_BUILDER_ROOT/libvirt/images** folder:
 * ws-2012-eval.iso - Windows installation ISO file. Must be renamed or simlinked.
 * virtio-win-0.1-52.iso - VirtIO drivers for Windows.
 
-Files that **MUST** be placed under **$BUILD_ROOT/share/files** are described in README file under **share/files** subfolder.
+Files that **MUST** be placed under **$IMAGE_BUILDER_ROOT/share/files** are described in README file under **share/files** subfolder.
 
 Required steps
 --------------
 
-1. Run **make build-root** to create directory structure. It will be built under '/opt/image-builder' folder, which is internally referred by **BUILD_ROOT** variable.
-2. Copy prerequisite files to their folders.
-3. Run **make test-build-files** to ensure that all files are in place.
-4. Run **make** to show available image targets.
-5. Run **make <image target>** to build image.
-6. Image file should be saved under **$BUILD_ROOT/libvirt/images** folder.
+1. Run **install.sh** to install required prerequisites and configure system. This script will create folder structure, install required packages and configures Samba share required by build script.
+2. Run **make build-root** to update build root directory content.
+3. Copy prerequisite files to their folders.
+4. Run **make test-build-files** to ensure that all files are in place.
+5. Run **make** to show available image targets.
+6. Run **make <image target>** to build image.
+7. Image file should be saved under **$IMAGE_BUILDER_ROOT/libvirt/images** folder.
 
 * Run **make clean** to remove files produced by this makefile only. NOTE: 'static files' (prerequisites) will be kept.
 * Run **make clean-all** to run clean other files, that were prodiced by other makefiles.
--- image-builder/install.sh
+++ image-builder/install.sh
@@ -1,28 +1,188 @@
-#!/bin/bash
+#!/bin/bash -x
 
-apt-get install zip kvm libvirt-bin virtinst samba
+# Determine OS Vendor, Release and Update
+# Tested with OS/X, Ubuntu, RedHat, CentOS, Fedora
+# Returns results in global variables:
+# os_VENDOR - vendor name
+# os_RELEASE - release
+# os_UPDATE - update
+# os_PACKAGE - package type
+# os_CODENAME - vendor's codename for release
+# GetOSVersion
+GetOSVersion() {
+    # Figure out which vendor we are
+    if [[ -x "`which sw_vers 2>/dev/null`" ]]; then
+        # OS/X
+        os_VENDOR=`sw_vers -productName`
+        os_RELEASE=`sw_vers -productVersion`
+        os_UPDATE=${os_RELEASE##*.}
+        os_RELEASE=${os_RELEASE%.*}
+        os_PACKAGE=""
+        if [[ "$os_RELEASE" =~ "10.7" ]]; then
+            os_CODENAME="lion"
+        elif [[ "$os_RELEASE" =~ "10.6" ]]; then
+            os_CODENAME="snow leopard"
+        elif [[ "$os_RELEASE" =~ "10.5" ]]; then
+            os_CODENAME="leopard"
+        elif [[ "$os_RELEASE" =~ "10.4" ]]; then
+            os_CODENAME="tiger"
+        elif [[ "$os_RELEASE" =~ "10.3" ]]; then
+            os_CODENAME="panther"
+        else
+            os_CODENAME=""
+        fi
+    elif [[ -x $(which lsb_release 2>/dev/null) ]]; then
+        os_VENDOR=$(lsb_release -i -s)
+        os_RELEASE=$(lsb_release -r -s)
+        os_UPDATE=""
+        os_PACKAGE="rpm"
+        if [[ "Debian,Ubuntu,LinuxMint" =~ $os_VENDOR ]]; then
+            os_PACKAGE="deb"
+        elif [[ "SUSE LINUX" =~ $os_VENDOR ]]; then
+            lsb_release -d -s | grep -q openSUSE
+            if [[ $? -eq 0 ]]; then
+                os_VENDOR="openSUSE"
+            fi
+        elif [[ $os_VENDOR == "openSUSE project" ]]; then
+            os_VENDOR="openSUSE"
+        elif [[ $os_VENDOR =~ Red.*Hat ]]; then
+            os_VENDOR="Red Hat"
+        fi
+        os_CODENAME=$(lsb_release -c -s)
+    elif [[ -r /etc/redhat-release ]]; then
+        # Red Hat Enterprise Linux Server release 5.5 (Tikanga)
+        # Red Hat Enterprise Linux Server release 7.0 Beta (Maipo)
+        # CentOS release 5.5 (Final)
+        # CentOS Linux release 6.0 (Final)
+        # Fedora release 16 (Verne)
+        # XenServer release 6.2.0-70446c (xenenterprise)
+        os_CODENAME=""
+        for r in "Red Hat" CentOS Fedora XenServer; do
+            os_VENDOR=$r
+            if [[ -n "`grep \"$r\" /etc/redhat-release`" ]]; then
+                ver=`sed -e 's/^.* \([0-9].*\) (\(.*\)).*$/\1\|\2/' /etc/redhat-release`
+                os_CODENAME=${ver#*|}
+                os_RELEASE=${ver%|*}
+                os_UPDATE=${os_RELEASE##*.}
+                os_RELEASE=${os_RELEASE%.*}
+                break
+            fi
+            os_VENDOR=""
+        done
+        os_PACKAGE="rpm"
+    elif [[ -r /etc/SuSE-release ]]; then
+        for r in openSUSE "SUSE Linux"; do
+            if [[ "$r" = "SUSE Linux" ]]; then
+                os_VENDOR="SUSE LINUX"
+            else
+                os_VENDOR=$r
+            fi
 
-mkdir -p /opt/image-builder/share/files
-mkdir -p /opt/image-builder/share/images
-mkdir -p /opt/image-builder/share/scripts
-mkdir -p /opt/image-builder/libvirt/images
+            if [[ -n "`grep \"$r\" /etc/SuSE-release`" ]]; then
+                os_CODENAME=`grep "CODENAME = " /etc/SuSE-release | sed 's:.* = ::g'`
+                os_RELEASE=`grep "VERSION = " /etc/SuSE-release | sed 's:.* = ::g'`
+                os_UPDATE=`grep "PATCHLEVEL = " /etc/SuSE-release | sed 's:.* = ::g'`
+                break
+            fi
+            os_VENDOR=""
+        done
+        os_PACKAGE="rpm"
+    # If lsb_release is not installed, we should be able to detect Debian OS
+    elif [[ -f /etc/debian_version ]] && [[ $(cat /proc/version) =~ "Debian" ]]; then
+        os_VENDOR="Debian"
+        os_PACKAGE="deb"
+        os_CODENAME=$(awk '/VERSION=/' /etc/os-release | sed 's/VERSION=//' | sed -r 's/\"|\(|\)//g' | awk '{print $2}')
+        os_RELEASE=$(awk '/VERSION_ID=/' /etc/os-release | sed 's/VERSION_ID=//' | sed 's/\"//g')
+    fi
+    export os_VENDOR os_RELEASE os_UPDATE os_PACKAGE os_CODENAME
+}
 
-if grep -q 'image-builder-share' /etc/samba/smb.conf ; then
-    echo "Samba configuration already applied"
-else
-    echo "Updating samba configuration"
-    cat << EOF >> /etc/samba/smb.conf
 
-[image-builder-share]
-    comment = Image Builder Share
-    path = /opt/image-builder/share
-    browsable = yes
-    guest ok = yes
-    guest account = nobody
-    read only = no
-    create mask = 0755
-EOF
+# Get an option from an INI file
+# iniget config-file section option
+function iniget() {
+    local file=$1
+    local section=$2
+    local option=$3
+    local line
+    line=$(sed -ne "/^\[$section\]/,/^\[.*\]/ { /^$option[ \t]*=/ p; }" "$file")
+    echo ${line#*=}
+}
 
-    restart smbd
-    restart nmbd
-fi
+
+# Determinate is the given option present in the INI file
+# ini_has_option config-file section option
+function ini_has_option() {
+    local file=$1
+    local section=$2
+    local option=$3
+    local line
+    line=$(sed -ne "/^\[$section\]/,/^\[.*\]/ { /^$option[ \t]*=/ p; }" "$file")
+    [ -n "$line" ]
+}
+
+
+# Set an option in an INI file
+# iniset config-file section option value
+function iniset() {
+    local file=$1
+    local section=$2
+    local option=$3
+    local value=$4
+
+    [[ -z $section || -z $option ]] && return
+
+    if ! grep -q "^\[$section\]" "$file" 2>/dev/null; then
+        # Add section at the end
+        echo -e "\n[$section]" >>"$file"
+    fi
+    if ! ini_has_option "$file" "$section" "$option"; then
+        # Add it
+        sed -i -e "/^\[$section\]/ a\\
+$option = $value
+" "$file"
+    else
+        local sep=$(echo -ne "\x01")
+        # Replace it
+        sed -i -e '/^\['${section}'\]/,/^\[.*\]/ s'${sep}'^\('${option}'[ \t]*=[ \t]*\).*$'${sep}'\1'"${value}"${sep} "$file"
+    fi
+}
+
+
+GetOSVersion
+
+
+case $os_VENDOR in
+	'Red Hat'|'CentOS')
+		#yum install -y
+	;;
+	'Debian'|'Ubuntu')
+		apt-get install --yes zip kvm libvirt-bin virtinst samba
+	;;
+	*)
+	;;
+esac
+
+
+SAMBA_CONF=/etc/samba/smb.conf
+IMAGE_BUILDER_ROOT=/opt/image-builder
+
+
+mkdir -p $IMAGE_BUILDER_ROOT/share/files
+mkdir -p $IMAGE_BUILDER_ROOT/share/images
+mkdir -p $IMAGE_BUILDER_ROOT/share/scripts
+mkdir -p $IMAGE_BUIDLER_ROOT/libvirt/images
+
+
+iniset $SAMBA_CONF "image-builder-share" "comment" "Image Builder Share"
+iniset $SAMBA_CONF "image-builder-share" "path" "$IMAGE_BUILDER_ROOT/share"
+iniset $SAMBA_CONF "image-builder-share" "browsable" "yes"
+iniset $SAMBA_CONF "image-builder-share" "guest ok" "yes"
+iniset $SAMBA_CONF "image-builder-share" "guest account" "nobody"
+iniset $SAMBA_CONF "image-builder-share" "read only" "no"
+iniset $SAMBA_CONF "image-builder-share" "create mask" "0755"
+
+
+restart smbd
+restart nmbd
+
