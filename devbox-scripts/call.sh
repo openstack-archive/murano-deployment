e1cfc3cb225ea59480915ff30654315b8df9a62a
Dmitry Teselkin <dteselkin@mirantis.com>
2014-02-06 15:21:03 +0400

    Scripts to install Murano from OBS packages
    !!! DO NOT MERGE, WORK IN PROGRESS !!!
    
    This scripts simplifies Murano installation process when it is required to
       install it from packages in development.
    
    It'll be updated to support Murano configuration tasks as well.
    
    Change-Id: I470ff21973d946a0352c6bbfe89fafa0dab10329

---
+++ devbox-scripts/call.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+func=${1:-''}
+shift
+
+
+# Import local configuration
+if [ -f '/etc/murano-deployment/obs-config.local' ]; then
+    source /etc/murano-deployment/obs-config.local
+fi
+ 
+source ./functions.sh
+source ./muranorc
+
+
+if [[ -n "$func" ]]; then
+	$func "$@"
+fi
+
