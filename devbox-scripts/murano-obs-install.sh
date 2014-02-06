bac31cccce31d16994a5445f4e4f886ca76fe64c
Dmitry Teselkin <dteselkin@mirantis.com>
2014-02-06 15:21:03 +0400

    Scripts to install Murano from OBS packages
    !!! DO NOT MERGE, WORK IN PROGRESS !!!
    
    This scripts simplifies Murano installation process when it is required to
       install it from packages in development.
    
    It'll be updated to support Murano configuration tasks as well.
    
    Change-Id: I470ff21973d946a0352c6bbfe89fafa0dab10329

---
+++ devbox-scripts/murano-obs-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -A ARGS
+ARGS=($@)
+
+source ./functions.sh
+
+
+# Import local configuration
+if [ -f '/etc/murano-deployment/obs-config.local' ]; then
+	source /etc/murano-deployment/obs-config.local
+fi
+
+
+# Verify some variables
+die_if_not_set $LINENO OBS_URL_PREFIX
+die_if_not_set $LINENO OBS_REPO_PREFIX
+die_if_not_set $LINENO OBS_LOCAL_REPO
+
+
+# Fiest, add a repo with all dependencies
+add_obs_repo
+
+# Then, remove all prefiously registered repos
+clean_obs_repo
+
+# Then, add per-requiest repositories
+for id in $OBS_REQUEST_IDS; do
+	add_obs_repo "$id"
+done
+
+# Update cache before actual installation
+apt-get update
