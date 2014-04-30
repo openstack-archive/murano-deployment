286b6208331b74bff9ef5479d5d9bf7f8e0b2a19
Sergey Kolekonov <skolekonov@mirantis.com>
2014-04-30 16:20:52 +0400

    Fix murano-dashboard-integration-tests job
    - OpenStack address was changed
    - Tests are executed on Jenkins slaves now
      so a virtual display server must be run on it
    
    Change-Id: I010e14d08b86d2b02bff7517ac15edddcfc99776

--- murano-ci/scripts/murano-dashboard-integration-tests.sh
+++ murano-ci/scripts/murano-dashboard-integration-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd $WORKSPACE
 
+export DISPLAY=:22
+screen -dmS display sudo Xvfb -fp /usr/share/fonts/X11/misc/ :22 -screen 0 1024x768x16
 sudo iptables -F
 sudo ntpdate pool.ntp.org
 sudo su -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
@@ -10,11 +12,11 @@ git clone https://git.openstack.org/stackforge/murano-tests
 
 python murano-ci/infra/RabbitMQ.py -username murano$BUILD_NUMBER -vhostname murano$BUILD_NUMBER
 
-sudo bash -x murano-ci/infra/deploy_component_new.sh $ZUUL_REF murano-dashboard 172.18.124.203 $ZUUL_URL
-sudo bash -x murano-ci/infra/configure_api.sh 172.18.124.203 5672 False murano$BUILD_NUMBER
+sudo bash -x murano-ci/infra/deploy_component_new.sh $ZUUL_REF murano-dashboard 172.18.11.4 $ZUUL_URL
+sudo bash -x murano-ci/infra/configure_api.sh 172.18.11.4 5672 False murano$BUILD_NUMBER
 
 cd murano-tests/muranodashboard-tests
-sed "s%keystone_url = http://127.0.0.1:5000/v2.0/%keystone_url = http://172.18.124.203:5000/v2.0/%g" -i config/config_file.conf
+sed "s%keystone_url = http://127.0.0.1:5000/v2.0/%keystone_url = http://172.18.11.4:5000/v2.0/%g" -i config/config_file.conf
 sed "s%horizon_url = http://127.0.0.1/horizon%horizon_url = http://$ADDR/horizon%g" -i config/config_file.conf
 sed "s%murano_url = http://127.0.0.1:8082%murano_url = http://$ADDR:8082%g" -i config/config_file.conf
 
