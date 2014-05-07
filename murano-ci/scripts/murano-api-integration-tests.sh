e1a659194297a087d727fbddc6837d099c60cd16
Sergey Kolekonov <skolekonov@mirantis.com>
2014-05-07 12:27:56 +0400

    Fix murano-dashboard and murano-api Jenkins jobs
    - added correct RabbitMQ url
    
    Change-Id: I4939da143fca5a8a93c3e42a4baba675beb5f1ba

--- murano-ci/scripts/murano-api-integration-tests.sh
+++ murano-ci/scripts/murano-api-integration-tests.sh
@@ -5,10 +5,10 @@ cd $WORKSPACE
 sudo ntpdate pool.ntp.org
 sudo su -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
 
-python murano-ci/infra/RabbitMQ.py -username murano$BUILD_NUMBER -vhostname murano$BUILD_NUMBER
+python murano-ci/infra/RabbitMQ.py -username murano$BUILD_NUMBER -vhostname murano$BUILD_NUMBER -rabbitmq_url 172.18.11.4:15672
 
 sudo bash -x murano-ci/infra/deploy_component_new.sh $ZUUL_REF murano-api noop $ZUUL_URL
-sudo bash -x murano-ci/infra/configure_api.sh 172.18.124.203 5672 False murano$BUILD_NUMBER
+sudo bash -x murano-ci/infra/configure_api.sh 172.18.11.4 5672 False murano$BUILD_NUMBER
 
 git clone https://github.com/Mirantis/tempest
 cd tempest
