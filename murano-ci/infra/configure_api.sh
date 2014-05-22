94d4dadba1a850c93bf4582e8d0ce3475ea34815
Igor Yozhikov <iyozhikov@mirantis.com>
2014-05-22 14:57:26 +0400

    Refactor scripts
    * Add common functions library
    * Refactor configure_api.sh
    
    Change-Id: Id5ff09d4ef7c213db710a03db0e30132f248b4b5

--- murano-ci/infra/configure_api.sh
+++ murano-ci/infra/configure_api.sh
@@ -1,22 +1,99 @@
 #!/bin/bash
+#
+CI_ROOT_DIR=$(cd $(dirname "$0") && cd .. && pwd)
+#Include of the common functions library file:
+INC_FILE="${CI_ROOT_DIR}/scripts/common.inc"
+if [ -f "$INC_FILE" ]; then
+    source "$INC_FILE"
+else
+    echo "Can't load \"$INC_FILE\" or file not found, exiting!"
+    exit 1
+fi
+#Basic parameters:
+DAEMON_USER="murano"
+DAEMON_GROUP="murano"
+DAEMON_CONF="/etc/murano/murano.conf"
+MANAGE_CMD=$(which murano-manage)
+#Set up this variable if necessary like RUN_DB_SYNC=true configure_api.sh param0 paramN
+RUN_DB_SYNC=${RUN_DB_SYNC:-false}
+#Functions:
+function check_prerequisites()
+{
+    local retval=0
+    if [ ! -f "$DAEMON_CONF" ]; then
+        echo "$DAEMON_CONF not found!${warn_message}"
+        retval=1
+    fi
+    getent group $DAEMON_GROUP > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "System group $DAEMON_GROUP not found!"
+        retval=1
+    fi
+    getent passwd $daemonuser > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "System user $DAEMON_USER not found!"
+        retval=1
+    fi
+    if [ $(sudo find /etc/init.d/ -name "murano-*" | wc -l) -ne 2 ]; then
+        echo "Check that /etc/init.d contains of SysV init scripts named \"murano-api\" and \"murano-engine\"!"
+        retval=1
+    fi
+    if [ ! -f "$MANAGE_CMD" ]; then
+        echo "murano-manage not found!"
+        retval=1
+    fi
+    return $retval
+}
 
-sed -i "s/#amqp_durable_queues=false/amqp_durable_queues=false/" /etc/murano/murano.conf
-sed -i "s/#amqp_auto_delete=false/amqp_auto_delete=false/" /etc/murano/murano.conf
-sed -i "s/#rabbit_host=localhost/rabbit_host=$1/" /etc/murano/murano.conf
-sed -i "s/#rabbit_port=5672/rabbit_port=$2/"  /etc/murano/murano.conf
-sed -i 's/#\\(rabbit_hosts=.*\\)/\\1/' /etc/murano/murano.conf
-sed -i "s/#rabbit_use_ssl=false/rabbit_use_ssl=$3/" /etc/murano/murano.conf
-sed -i "s/#rabbit_userid=guest/rabbit_userid=$4/" /etc/murano/murano.conf
-sed -i "s/#rabbit_password=guest/rabbit_password=swordfish/" /etc/murano/murano.conf
-sed -i "s/#rabbit_virtual_host=\\//rabbit_virtual_host=$4/" /etc/murano/murano.conf
-sed -i "s/#rabbit_retry_interval=1/rabbit_retry_interval=1/" /etc/murano/murano.conf
-sed -i "s/#rabbit_retry_backoff=2/rabbit_retry_backoff=2/" /etc/murano/murano.conf
-sed -i "s/#rabbit_max_retries=0/rabbit_max_retries=0/" /etc/murano/murano.conf
-sed -i "s/#rabbit_ha_queues=false/rabbit_ha_queues=false/" /etc/murano/murano.conf
-sed -i "s/auth_host = 127.0.0.1/auth_host = $1/" /etc/murano/murano.conf
-sed -i "s/connection = sqlite:\\/\\/\\/\\/etc\\/murano\\/murano-api.sqlite/connection = mysql:\\/\\/murano:swordfish@localhost:3306\\/murano/" /etc/murano/murano.conf
-sed -i "s/auth_url = http:\\/\\/localhost:5000\\/v2.0/auth_url = http:\\/\\/$1:5000\\/v2.0/" /etc/murano/murano.conf
+function configure_api()
+{
+    local retval=0
 
-sudo -u murano murano-manage --config-file /etc/murano/murano.conf db-sync
-service murano-api restart
-service murano-engine restart
+    iniset 'DEFAULT' 'amqp_auto_delete' "false" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'amqp_durable_queues' "false" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'amqp_auto_delete' "false" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_retry_interval' "1" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_retry_backoff' "2" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_max_retries' "0" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_ha_queues' "false" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_max_retries' "0" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_virtual_host' "$RMQ_VHOST" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_password' "$RMQ_PASSWD" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_userid' "$RMQ_USER" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_use_ssl' "$RMQ_SSL" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_port' "$RMQ_PORT" "$DAEMON_CONF"
+    iniset 'DEFAULT' 'rabbit_host' "$REMOTE_HOST" "$DAEMON_CONF"
+
+    iniset 'keystone_authtoken' 'auth_host' "$REMOTE_HOST" "$DAEMON_CONF"
+
+    iniset 'keystone' 'auth_url' "http:\/\/$REMOTE_HOST\:5000\/v2.0" "$DAEMON_CONF"
+
+    iniset 'rabbitmq' 'virtual_host' "$RMQ_VHOST" "$DAEMON_CONF"
+    iniset 'rabbitmq' 'password' "$RMQ_PASSWD" "$DAEMON_CONF"
+    iniset 'rabbitmq' 'login' "$RMQ_USER" "$DAEMON_CONF"
+    iniset 'rabbitmq' 'port' "$RMQ_PORT" "$DAEMON_CONF"
+    iniset 'rabbitmq' 'host' "$REMOTE_HOST" "$DAEMON_CONF"
+
+    return $retval
+}
+
+#Staring up:
+if [ ! $# -ge 5 ]; then
+    echo "Usage: $(basename $0) os[rabbitmq]_host rabbitmq_port rabbitmq_usessl rabbitmq_userid rabbitmq_vhost"
+    exit 1
+else
+    readonly REMOTE_HOST=$1
+    readonly RMQ_PORT=$2
+    readonly RMQ_SSL=$3
+    readonly RMQ_USER=$4
+    readonly RMQ_PASSWD="swordfish"
+    readonly RMQ_VHOST=$5
+fi
+check_prerequisites || exit $?
+configure_api || exit $?
+if [ "$RUN_DB_SYNC" == true ]; then
+    echo "su -c \"$MANAGE_CMD --config-file $DAEMON_CONF import-package db-sync\" -s /bin/bash $DAEMON_USER 2>&1"
+fi
+service murano-api restart || exit $?
+service murano-engine restart || exit $?
+exit 0
