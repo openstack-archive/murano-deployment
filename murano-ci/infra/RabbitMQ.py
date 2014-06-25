0de0e51c361bda81909ce18e4f55ebde53c63715
Igor Yozhikov <iyozhikov@mirantis.com>
2014-06-25 16:43:46 +0400

    Refactored RabbitMQ.py
    * suppressed html output from rabbitmq management
    * add functions
    
    Change-Id: I27259500efb18ea7e9a63ce60920af5824ffbd48

--- murano-ci/infra/RabbitMQ.py
+++ murano-ci/infra/RabbitMQ.py
@@ -1,11 +1,14 @@
-from pyrabbit.api import Client
+from pyrabbit import api
 import argparse
-
+import sys
+# Global variables and command line parsing
 parser = argparse.ArgumentParser(description="Script for creating rabbitmq"
                                              "users and vhost for jenkins's"
                                              "jobs")
-parser.add_argument("-rabbitmq_url",  dest='rabbitmq_url',  type=str,
-                    help="URL of using RabbitMQ", default='172.18.124.203:55672')
+parser.add_argument("-rabbitmq_host",  dest='rabbitmq_host',  type=str,
+                    help="Host of RabbitMQ", default='172.18.124.203')
+parser.add_argument("-rabbitmq_port",  dest='rabbitmq_port',  type=str,
+                    help="Management port of RabbitMQ", default='15672')
 parser.add_argument("-rabbitmq_username",  dest='rabbitmq_username',  type=str,
                     help="Username for RabbitMQ auth",  default='guest')
 parser.add_argument("-rabbitmq_password",  dest='rabbitmq_password',  type=str,
@@ -19,48 +22,111 @@ parser.add_argument("-vhostname",  dest='vhostname',  type=str,
 parser.add_argument("-action",  dest='action',  type=str,
                     help="create/delete",  default='create')
 args = parser.parse_args()
-rabbitmq_url = args.rabbitmq_url
+rabbitmq_host = args.rabbitmq_host
+rabbitmq_port = args.rabbitmq_port
+rabbitmq_url = str(rabbitmq_host+":"+rabbitmq_port)
 rabbitmq_user = args.rabbitmq_username
 rabbitmq_password = args.rabbitmq_password
 user = args.username
 password = args.password
 vhost = args.vhostname
 action = args.action
+#
+# Functions
+#
+class DevNull(object):
+    def write(self, data): pass
 
-cl = Client(rabbitmq_url, rabbitmq_user, rabbitmq_password)
-assert cl.is_alive()
+# suppress console output
+_stdout = None
+def mute_stdout():
+    global _stdout
+    if _stdout is None:
+        _stdout = sys.stdout
+        sys.stdout = DevNull()
 
+# restore console output
+def unmute_stdout():
+   global _stdout
+   if _stdout is not None:
+       sys.stdout = _stdout
+       _stdout = None
+
+# checnk rabbitmq connection state
+def check_connection():
+    try:
+        assert cl.is_alive()
+    except Exception, err:
+        print("Can't connect to '%s'" % rabbitmq_url)
+        exit(1)
+    return True
+
+# check vhost existence
+def check_vhost_exists(vhost_name):
+    try:
+        mute_stdout()
+        cl.get_vhost(vhost)
+    except Exception, err:
+        unmute_stdout()
+        if err.status == 404:
+            print("No vhost named '%s'" % vhost_name)
+            return False
+    unmute_stdout()
+    return True
+
+# queues cleanup
+def clean_queues(vhost_name):
+    owner=api.Client(rabbitmq_url, user, password)
+    vhost_queues=owner.get_queues(vhost_name)
+    if vhost_queues.count > 0:
+        for vhost_queue in vhost_queues:
+            try:
+                owner.purge_queue(vhost_name,vhost_queue['name'])
+            except Exception, err:
+                print("Purge queue '%s' from vhost '%s' fails" % (vhost_queue['name'],vhost_name))
+            try:
+                owner.delete_queue(vhost_name, vhost_queue['name'])
+            except Exception, err:
+                print("Delete queue '%s' from vhost '%s' fails" % (vhost_queue['name'],vhost_name))
+    else:
+        print("Queues absent in vhost '%s'" % vhost_name)
+
+# remove rabbitmq endpoint
+def remove_rabbit_endpoint(vhost_name,vhost_owner):
+    print("Deleting vhost '%s' and user '%s'" % (vhost_name,vhost_owner))
+    try:
+        cl.delete_user(vhost_owner)
+    except Exception, err:
+        print("User '%s' deletion fails" % vhost_owner)
+        exit(1)
+    try:
+        cl.delete_vhost(vhost_name)
+    except Exception, err:
+        print("Vhost '%s' deletion fails" % vhost_name)
+        exit(1)
+
+# add rabbitmq endpoint
+def create_rabbit_endpoint(vhost_name, vhost_owner, vhost_password):
+    print("Creating vhost '%s' and user '%s'" % (vhost_name,vhost_owner))
+    try:
+        cl.create_vhost(vhost_name)
+        cl.create_user(vhost_owner, vhost_password, tags='administrator')
+        cl.set_vhost_permissions(vhost_name, vhost_owner, '.*', '.*', '.*')
+    except Exception, err:
+        print(err)
+        exit(1)
+
+# Main runtime
 print("-" * 30)
 print("This script will configure RabbitMQ server for Murano."
       "If something can't be deleted, probably it doesn't exist.")
-
-for queue in cl.get_queues():
-    if queue['vhost'] == vhost:
-        try:
-            cl.purge_queue(vhost, queue['name'])
-            cl.delete_queue(vhost, queue['name'])
-        except:
-            msg = "Can't delete queue {0} in RabbitMQ host {1}"
-            print(msg.format(queue['name'], rabbitmq_url))
-
-try:
-    cl.delete_vhost(vhost)
-except:
-    msg = "Can't delete vhost {0} in RabbitMQ host {1}"
-    print(msg.format(vhost, rabbitmq_url))
-
-try:
-    cl.delete_user(user)
-except:
-    msg = "Can't delete user {0} in RabbitMQ host {1}"
-    print(msg.format(user, rabbitmq_url))
-
+cl = api.Client(rabbitmq_url, rabbitmq_user, rabbitmq_password)
+check_connection()
+if check_vhost_exists(vhost):
+    print("vhost named '%s' exists and will be deleted" % vhost)
+    clean_queues(vhost)
+    remove_rabbit_endpoint(vhost,user)
 if action == 'create':
-    try:
-        cl.create_vhost(vhost)
-        cl.create_user(user, password, tags='administrator')
-        cl.set_vhost_permissions(vhost, user, '.*', '.*', '.*')
-    except:
-        print("Can't init new RabbitMQ vhost and user!")
-
+    create_rabbit_endpoint(vhost, user, password)
 print("-" * 30)
+exit(0)
