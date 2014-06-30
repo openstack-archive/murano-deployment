1f522af8382b9d4b4688e173893f73b89c9c5e38
Igor Yozhikov <iyozhikov@mirantis.com>
2014-06-30 17:00:35 +0400

    Add collect_artifacts to all jobs
    * add collect_artifacts shell functions to jobs
    * add AMPQ non existant user handling
    * add reties on creation/deleting of the rabbitmq endpoints
    This changeset handle remote rabbitmq server operations during jenkins job run.
    Also add logs and another artifacts collection as on success as on fail of the job.
    
    Change-Id: I3e51067bcfb1527794661fe6a952e174b6d928d4

--- murano-ci/infra/RabbitMQ.py
+++ murano-ci/infra/RabbitMQ.py
@@ -1,30 +1,31 @@
 from pyrabbit import api
 import argparse
 import sys
+import time
 # Global variables and command line parsing
 parser = argparse.ArgumentParser(description="Script for creating rabbitmq"
                                              "users and vhost for jenkins's"
                                              "jobs")
-parser.add_argument("-rabbitmq_host",  dest='rabbitmq_host',  type=str,
+parser.add_argument("-rabbitmq_host", dest='rabbitmq_host', type=str,
                     help="Host of RabbitMQ", default='localhost')
-parser.add_argument("-rabbitmq_port",  dest='rabbitmq_port',  type=str,
+parser.add_argument("-rabbitmq_port", dest='rabbitmq_port', type=str,
                     help="Management port of RabbitMQ", default='55672')
-parser.add_argument("-rabbitmq_username",  dest='rabbitmq_username',  type=str,
-                    help="Username for RabbitMQ auth",  default='guest')
-parser.add_argument("-rabbitmq_password",  dest='rabbitmq_password',  type=str,
-                    help="Password for RabbitMQ auth",  default='guest')
-parser.add_argument("-username",  dest='username',  type=str,
-                    help="Username",  default='test')
-parser.add_argument("-password",  dest='password',  type=str,
-                    help="Password",  default='swordfish')
-parser.add_argument("-vhostname",  dest='vhostname',  type=str,
-                    help="Vhost name",  default='test')
-parser.add_argument("-action",  dest='action',  type=str,
-                    help="create/delete",  default='create')
+parser.add_argument("-rabbitmq_username", dest='rabbitmq_username', type=str,
+                    help="Username for RabbitMQ auth", default='guest')
+parser.add_argument("-rabbitmq_password", dest='rabbitmq_password', type=str,
+                    help="Password for RabbitMQ auth", default='guest')
+parser.add_argument("-username", dest='username', type=str,
+                    help="Username", default='test')
+parser.add_argument("-password", dest='password', type=str,
+                    help="Password", default='swordfish')
+parser.add_argument("-vhostname", dest='vhostname', type=str,
+                    help="Vhost name", default='test')
+parser.add_argument("-action", dest='action', type=str,
+                    help="create/delete", default='create')
 args = parser.parse_args()
 rabbitmq_host = args.rabbitmq_host
 rabbitmq_port = args.rabbitmq_port
-rabbitmq_url = str(rabbitmq_host+":"+rabbitmq_port)
+rabbitmq_url = str(rabbitmq_host + ":" + rabbitmq_port)
 rabbitmq_user = args.rabbitmq_username
 rabbitmq_password = args.rabbitmq_password
 user = args.username
@@ -34,23 +35,31 @@ action = args.action
 #
 # Functions
 #
+
+
 class DevNull(object):
-    def write(self, data): pass
+    def write(self, data):
+        pass
+
 
 # suppress console output
 _stdout = None
+
+
 def mute_stdout():
     global _stdout
     if _stdout is None:
         _stdout = sys.stdout
         sys.stdout = DevNull()
 
+
 # restore console output
 def unmute_stdout():
-   global _stdout
-   if _stdout is not None:
-       sys.stdout = _stdout
-       _stdout = None
+    global _stdout
+    if _stdout is not None:
+        sys.stdout = _stdout
+        _stdout = None
+
 
 # check rabbitmq connection state
 def check_connection():
@@ -61,18 +70,20 @@ def check_connection():
         exit(1)
     return True
 
+
 # check management context port
 def check_mgmt_ctx_port():
     global rabbitmq_url
     global cl
     try:
-        overview=cl.get_overview()
-        rabbitmq_url=str(rabbitmq_host+":"+str(overview['contexts'][0]['port']))
+        overview = cl.get_overview()
+        rabbitmq_url = str(rabbitmq_host + ":" + str(overview['contexts'][0]['port']))
         cl = api.Client(rabbitmq_url, rabbitmq_user, rabbitmq_password)
     except Exception, err:
         print(err)
         exit(1)
     check_connection()
+
 
 # check vhost existence
 def check_vhost_exists(vhost_name):
@@ -87,47 +98,99 @@ def check_vhost_exists(vhost_name):
     unmute_stdout()
     return True
 
+
 # queues cleanup
 def clean_queues(vhost_name):
-    owner=api.Client(rabbitmq_url, user, password)
-    vhost_queues=owner.get_queues(vhost_name)
+    owner = api.Client(rabbitmq_url, user, password)
+    try:
+        vhost_queues = owner.get_queues(vhost_name)
+    except:
+        print("Something wrong with credentials, can't access queues in the vhost '%s'" % vhost_name)
+        return
     if vhost_queues.count > 0:
         for vhost_queue in vhost_queues:
             try:
-                owner.purge_queue(vhost_name,vhost_queue['name'])
+                owner.purge_queue(vhost_name, vhost_queue['name'])
             except Exception, err:
-                print("Purge queue '%s' from vhost '%s' fails" % (vhost_queue['name'],vhost_name))
+                print("Purge queue '%s' from vhost '%s' fails" % (vhost_queue['name'], vhost_name))
             try:
                 owner.delete_queue(vhost_name, vhost_queue['name'])
             except Exception, err:
-                print("Delete queue '%s' from vhost '%s' fails" % (vhost_queue['name'],vhost_name))
+                print("Delete queue '%s' from vhost '%s' fails" % (vhost_queue['name'], vhost_name))
     else:
         print("Queues absent in vhost '%s'" % vhost_name)
 
+
 # remove rabbitmq endpoint
-def remove_rabbit_endpoint(vhost_name,vhost_owner):
-    print("Deleting vhost '%s' and user '%s'" % (vhost_name,vhost_owner))
-    try:
-        cl.delete_user(vhost_owner)
-    except Exception, err:
-        print("User '%s' deletion fails" % vhost_owner)
+def remove_rabbit_endpoint(vhost_name, vhost_owner):
+    retries = 2
+    ret_interval = 2
+    print("Deleting vhost '%s' and user '%s'" % (vhost_name, vhost_owner))
+    for attempt in range(retries):
+        try:
+            mute_stdout()
+            cl.delete_user(vhost_owner)
+        except Exception, err:
+            unmute_stdout()
+            print("User '%s' deletion fails or user doesn't exists, making one more attempt '%s'" % (
+                vhost_owner, (attempt + 1)))
+            time.sleep(ret_interval)
+        else:
+            unmute_stdout()
+            print("...user '%s' deleted" % vhost_owner)
+            break
+    for attempt in range(retries):
+        try:
+            cl.delete_vhost(vhost_name)
+        except Exception, err:
+            unmute_stdout()
+            print("Error occurred:'%s', making one more attempt '%s'" % (err, (attempt + 1)))
+            time.sleep(ret_interval)
+        else:
+            unmute_stdout()
+            print("...vhost '%s' deleted" % vhost_name)
+            break
+    else:
+        print("Vhost '%s' deletion fails, max attempts reached" % vhost_name)
         exit(1)
-    try:
-        cl.delete_vhost(vhost_name)
-    except Exception, err:
-        print("Vhost '%s' deletion fails" % vhost_name)
-        exit(1)
+
 
 # add rabbitmq endpoint
 def create_rabbit_endpoint(vhost_name, vhost_owner, vhost_password):
-    print("Creating vhost '%s' and user '%s'" % (vhost_name,vhost_owner))
-    try:
-        cl.create_vhost(vhost_name)
-        cl.create_user(vhost_owner, vhost_password, tags='administrator')
-        cl.set_vhost_permissions(vhost_name, vhost_owner, '.*', '.*', '.*')
-    except Exception, err:
-        print(err)
+    retries = 4
+    ret_interval = 2
+    print("Creating vhost '%s' and user '%s'" % (vhost_name, vhost_owner))
+    for attempt in range(retries):
+        try:
+            cl.create_vhost(vhost_name)
+        except Exception, err:
+            print("Error occurred:'%s', making one more attempt '%s'" % (err, (attempt + 1)))
+            time.sleep(ret_interval)
+        else:
+            print("...vhost created")
+            break
+    for attempt in range(retries):
+        try:
+            cl.create_user(vhost_owner, vhost_password, tags='administrator')
+        except Exception, err:
+            print("Error occurred:'%s', making one more attempt '%s'" % (err, (attempt + 1)))
+            time.sleep(ret_interval)
+        else:
+            print("...username and password set")
+            break
+    for attempt in range(retries):
+        try:
+            cl.set_vhost_permissions(vhost_name, vhost_owner, '.*', '.*', '.*')
+        except Exception, err:
+            print("Error occurred:'%s', making one more attempt '%s'" % (err, (attempt + 1)))
+            time.sleep(ret_interval)
+        else:
+            print("...permissions set")
+            break
+    else:
+        print("Max attempts reached, exiting!")
         exit(1)
+
 
 # Main runtime
 print("-" * 30)
@@ -139,7 +202,7 @@ check_mgmt_ctx_port()
 if check_vhost_exists(vhost):
     print("vhost named '%s' exists and will be deleted" % vhost)
     clean_queues(vhost)
-    remove_rabbit_endpoint(vhost,user)
+    remove_rabbit_endpoint(vhost, user)
 if action == 'create':
     create_rabbit_endpoint(vhost, user, password)
 print("-" * 30)
