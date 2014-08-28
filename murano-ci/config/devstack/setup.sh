079dbfdda4218c4a0967d0921b4860ed7dc55830
Dmitry Teselkin <dteselkin@mirantis.com>
2014-08-28 15:32:11 +0400

    Update devstack config
    Fix typos in local.conf
    Add heat_stack_owner role to ci user
    Fix ubuntu cloud image URL
    Add Fedora 20 cloud image
    Add workaround against https://bugs.launchpad.net/neutron/+bug/1263997
    Use default security group from CI tenant
    Typo
    Update user quotas
    Code style and readability
    Post unstack script
    Post stack script
    Add build script for image with Murano agent
    Add Readme
    Fix metadata issue (add use_forwarder_for)
    Update split-logs.sh to support devstack log files
    Add script to rotate devstack logs
    Update murano image build script
    Add murano heartbeat jobs
    Add kind of setup script
    
    Change-Id: I57a5566a14c66ec8e4378e3545344f0fb6a2617b

---
+++ murano-ci/config/devstack/setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+action=${1:-help}
+
+STACK_HOME=/opt/stack
+
+
+
+function install_devstack() {
+    pushd ${STACK_HOME}/devstack
+    cp ./post-stack.sh ${STACK_HOME}/devstack/
+    ./stack.sh
+    ./post-stack.sh
+    popd
+}
+
+function uninstall_devstack() {
+    pushd ${STACK_HOME}/devstack
+    cp ./post-unstack.sh ${STACK_HOME}/devstack/
+    ./unstack.sh
+    ./post-unstack.sh
+    popd
+}
+
+function prepare_lab() {
+    cp ../../tools/rotate-devstack-logs.sh ${STACK_HOME}
+    cp ../../tools/split-logs.sh ${STACK_HOME}
+
+    cp ./local.sh ${STACK_HOME}/devstack/
+    cp ./build-murano-image.sh ${STACK_HOME}/devstack/
+}
+
+function show_help() {
+    cat << EOF
+Available commands:
+
+    * stack - install devstack
+    * unstack - uninstall devstack
+EOF
+}
+
+
+
+case ${action} in
+    'stack')
+        prepare_lab
+        install_devstack
+    ;;
+    'unstack')
+        uninstall_devstack
+    ;;
+    'help')
+        show_help
+    ;;
+    *)
+        echo "Unknown command '${action}'"
+        show_help
+    ;;
+esac
