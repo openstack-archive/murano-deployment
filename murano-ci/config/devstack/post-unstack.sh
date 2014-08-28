f9a783f44f609be92181fe071e86a2f6257c0061
Dmitry Teselkin <dteselkin@mirantis.com>
2014-08-28 15:32:11 +0400

    Update devstack config
    Fix typos in local.conf
    Add heat_stack_owner role to ci user
    Fix ubuntu cloud image URL
    Add Fedora 20 cloud image
    Add workaround against https://bugs.launchpad.net/neutron/+bug/1263997
    Use default security group from CI tenant
    Typo
    Update user quotas
    Code style and readability
    Post unstack script
    Post stack script
    Add build script for image with Murano agent
    Add Readme
    
    Change-Id: I57a5566a14c66ec8e4378e3545344f0fb6a2617b

---
+++ murano-ci/config/devstack/post-unstack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source openrc admin admin
+
+instances_mount_point=/opt/stack/data/nova/instances
+
+for inst in $(sudo virsh list --all | tail -n +3 | awk '{print $2}'); do
+    sudo virsh destroy ${inst}
+    sudo virsh undefine ${inst}
+done
+
+sleep 5
+
+sudo service libvirt-bin stop
+
+sleep 5
+
+sudo killall -9 kvm
+
+sleep 5
+
+cd ${instances_mount_point} && sudo rm -rf *
+
+sleep 5
+
+sudo umount ${instances_mount_point}
+
+sudo service libvirt-bin start
+
+sudo route del default gw $NETWORK_GATEWAY dev $OVS_PHYSICAL_BRIDGE
+
