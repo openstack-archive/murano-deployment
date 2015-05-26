05fe6dc4a75d6ff341cb48d5f6915bc105abb4df
Kirill Zaitsev <kzaitsev@mirantis.com>
2015-05-26 17:07:55 +0300

    Set MURANO_REPO_URL for murano-dashboard tests
    Set MURANO_REPO_URL var during devstack configuration for dashboard and
    allow inbound traffic on 8099 port.
    
    Depends-On: I9ca36d1f5fb1ec9206465542a8af42522465735b
    Change-Id: I00b9e561ebc0c36f1a21e27d86179ffe9d41c97b

--- murano-ci/scripts/murano-integration-tests-devstack.sh
+++ murano-ci/scripts/murano-integration-tests-devstack.sh
@@ -517,6 +517,8 @@ function deploy_devstack() {
 
     cd "${STACK_HOME}/devstack"
 
+    # this is set only for murano-dashboard tests
+    INC_MURANO_REPO_URL=""
     case "${PROJECT_NAME}" in
         'murano')
             MURANO_REPO=${ZUUL_URL}/${ZUUL_PROJECT}
@@ -525,18 +527,21 @@ function deploy_devstack() {
         'murano-dashboard')
             MURANO_DASHBOARD_REPO=${ZUUL_URL}/${ZUUL_PROJECT}
             MURANO_DASHBOARD_BRANCH=${ZUUL_REF}
+            INC_MURANO_REPO_URL="MURANO_REPO_URL=\"http://${floating_ip_address}:8099\""
         ;;
         'python-muranoclient')
             MURANO_PYTHONCLIENT_REPO=${ZUUL_URL}/${ZUUL_PROJECT}
             MURANO_PYTHONCLIENT_BRANCH=${ZUUL_REF}
         ;;
     esac
+    echo "MURANO_REPO_URL=${MURANO_REPO_URL}"
     echo "MURANO_REPO=${MURANO_REPO}"
     echo "MURANO_BRANCH=${MURANO_BRANCH}"
     echo "MURANO_DASHBOARD_REPO=${MURANO_DASHBOARD_REPO}"
     echo "MURANO_DASHBOARD_BRANCH=${MURANO_DASHBOARD_BRANCH}"
     echo "MURANO_PYTHONCLIENT_REPO=${MURANO_PYTHONCLIENT_REPO}"
     echo "MURANO_PYTHONCLIENT_BRANCH=${MURANO_PYTHONCLIENT_BRANCH}"
+    echo "INC_MURANO_REPO_URL=${INC_MURANO_REPO_URL}"
 
 
     cat << EOF > local.conf
@@ -555,6 +560,7 @@ MURANO_DASHBOARD_REPO=${MURANO_DASHBOARD_REPO}
 MURANO_DASHBOARD_BRANCH=${MURANO_DASHBOARD_BRANCH}
 MURANO_PYTHONCLIENT_REPO=${MURANO_PYTHONCLIENT_REPO}
 MURANO_PYTHONCLIENT_BRANCH=${MURANO_PYTHONCLIENT_BRANCH}
+${INC_MURANO_REPO_URL}
 RABBIT_PASSWORD=guest
 MURANO_RABBIT_VHOST=/
 RECLONE=True
@@ -581,6 +587,7 @@ EOF
     sudo su -c "cd ${STACK_HOME}/devstack && ./stack.sh" stack
 
     # Fix iptables to allow outbound access
+    sudo iptables -I INPUT 1 -p tcp --dport 8099 -j ACCEPT
     sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
 }
 
