c3772bf101b241dd0caec3bdd1ec42732bff8741
Nikolay Starodubtsev <nstarodubtsev@mirantis.com>
2016-04-12 11:37:18 +0300

    Set log color to false
    Current log color implementation in devstack causes tput spam erros. As
    far as color log is not useful on CI nodes it was decided to skip
    colors.
    
    Change-Id: Ic78bbb963f6753301688d8126b8c2fe893ed95e7
    Closes-Bug: #1568833

--- murano-ci/scripts/deploy_devstack.sh
+++ murano-ci/scripts/deploy_devstack.sh
@@ -138,6 +138,7 @@ LIBS_FROM_GIT=${LIBS_FROM_GIT}
 RECLONE=True
 SCREEN_LOGDIR=/opt/stack/log/
 LOGFILE=\$SCREEN_LOGDIR/stack.sh.log
+LOG_COLOR=False
 ENABLED_SERVICES=
 enable_service mysql
 enable_service rabbit
