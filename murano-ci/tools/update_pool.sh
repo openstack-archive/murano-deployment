bf7147656ae691395106ef63dbf8fdc5a403e51b
Mykyta Karpin <mkarpin@mirantis.com>
2016-09-06 11:20:27 +0300

    Fix zuul config update
    Change-Id: If1453b4cd689df40baa363a15cd7cf13b4532a1d

--- murano-ci/tools/update_pool.sh
+++ murano-ci/tools/update_pool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-for i in $(sudo nodepool list | grep lab-cz5608 | awk -F '|' '{ print $2 }')
+for i in $(sudo nodepool list | grep cilab | awk -F '|' '{ print $2 }')
 do
    sudo nodepool delete $i
    sleep 2
